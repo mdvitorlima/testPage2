244e263186f3a583e1afdebcee4ad47ebf18644c
mdvitorlima <marciodvl@gmail.com>
2022-12-28 18:01:15 -0300

    .

--- index.js
+++ index.js
@@ -3,19 +3,25 @@
 /** @const */
 const vOpts = {
   dom: {
-    body: document.querySelector('body'),
-    start: document.querySelector('#inicio'),
-    readout: document.querySelector('#readout'),
-    version: document.querySelector('#jsVersion')
+    //start: document.querySelector('#inicio'),
+    //readout: document.querySelector('#readout'),
+    //version: document.querySelector('#jsVersion')
   },
-  watchId: null,
-  wakeLock: null
+  //watchId: null,
+  //wakeLock: null
 };
 
 var throttlePause;
 var velocidadeLimite = 30;
 const throttleRange = document.getElementById("throttle-range");
 const throttleTimeText = document.getElementById("throttle-time");
+
+const watchId = null;
+const wakeLock = null;
+
+const start = document.querySelector('#inicio');
+//const readout = document.querySelector('#readout');
+const version = document.querySelector('#jsVersion');
 
 
 window.addEventListener(
@@ -36,30 +42,31 @@ throttleRange.addEventListener(
 );
 
 document.querySelector('#inicio').addEventListener('click', (event) => {
-  if (vOpts.watchId) {
-    navigator.geolocation.clearWatch(vOpts.watchId);
+  if (watchId) {
+    navigator.geolocation.clearWatch(watchId);
 
-    if (vOpts.wakeLock) {
-      vOpts.wakeLock.cancel();
+    if (wakeLock) {
+      wakeLock.cancel();
     }
-
-    vOpts.watchId = null;
-    vOpts.dom.start.textContent = 'Iniciar';
-    vOpts.dom.start.classList.toggle('selected');
+    
+    watchId = null;
+    start.textContent = 'Iniciar';
+    start.classList.toggle('selected');
   } else {
     const options = {
       enableHighAccuracy: true
     };
-    vOpts.watchId = navigator.geolocation.watchPosition(atualizaPosicao,
+    
+    watchId = navigator.geolocation.watchPosition(atualizaPosicao,
       null, options);
 
     IniciarWakeLock();
     
-    vOpts.dom.start.textContent = 'Parar';
-    vOpts.dom.start.classList.toggle('selected');
+    start.textContent = 'Parar';
+    start.classList.toggle('selected');
   }
   
-  vOpts.dom.version.textContent = "2.5";
+  version.textContent = "2.5";
   
 });
 
@@ -67,7 +74,7 @@ document.querySelector('#inicio').addEventListener('click', (event) => {
 const IniciarWakeLock = () => {
   try {
     navigator.getWakeLock("screen").then((wakeLock) => {
-      vOpts.wakeLock = wakeLock.createRequest();
+      wakeLock = wakeLock.createRequest();
     });
   } catch(error) {
 
@@ -78,22 +85,20 @@ const atualizaPosicao = (position) => {
 
   let velocidade = position.coords.speed;
 
-  vOpts.dom.readout.textContent = Math.round(
-    velocidade * 3.6);
+  document.querySelector('#readout').textContent = Math.round(velocidade * 3.6);
     
     if(Math.round(velocidade * 3.6) > velocidadeLimite)
     {
       document.getElementById("divAlert").style.backgroundColor = "#FF0000";
       document.getElementById("divAlert").style.color = "#FFFFFF";
       document.getElementById("divAlert").style.fontSize = 22;      
-      //document.getElementById("divAlert").textContent = "VELOCIDADE MAX ATINGIDA";
-      document.getElementById("textAlert2").textContent = "VELOCIDADE MAX ATINGIDA 2";
+      document.getElementById("divAlert").textContent = "VELOCIDADE MAX ATINGIDA";
       beep(1000, 80, function () {});
     }
     else
     {
       document.getElementById("divAlert").style.backgroundColor = "#f1f1f1";
-      //document.getElementById("divAlert").textContent = "";
+      document.getElementById("divAlert").textContent = "";
     }
     
 };
@@ -103,8 +108,8 @@ document.getElementsByTagName("button")[1].addEventListener("click", function ()
 
       document.getElementById("divAlert").style.backgroundColor = "#FF0000";
       document.getElementById("divAlert").style.color = "#FFFFFF";
-      document.getElementById("divAlert").style.fontSize = 22;      
-      document.getElementById("textAlert2").textContent = "VELOCIDADE MAX ATINGIDA 2";
+      document.getElementById("divAlert").style.fontSize = 22; 
+      document.getElementById("divAlert").textContent = "VELOCIDADE MAX ATINGIDA";     
 });
 
 
